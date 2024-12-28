6ad40b533727b83d0dc6f6daef5976dd9152dbb0
GalacticPast <192919385+GalacticPast@users.noreply.github.com>
2024-12-28 19:20:09 -0300

    preliminary event system

---
+++ src/core/event.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "defines.h"
+
+typedef enum event_type
+{
+    EVENT_TYPE_ON_KEY_PRESS,
+    EVENT_TYPE_MAX_EVENT
+} event_type;
+
+typedef struct event
+{
+
+} event;
--- src/core/logger.h
+++ src/core/logger.h
@@ -2,18 +2,18 @@
 
 typedef enum log_levels
 {
-    FATAL,
-    ERROR,
-    DEBUG,
-    INFO,
-    TRACE,
-    MAX_LEVEL
+    LOG_LEVEL_FATAL,
+    LOG_LEVEL_ERROR,
+    LOG_LEVEL_DEBUG,
+    LOG_LEVEL_INFO,
+    LOG_LEVEL_TRACE,
+    LOG_LEVEL_MAX_LEVEL
 } log_levels;
 
 void log_message(log_levels level, const char *msg, ...);
 
-#define FATAL(msg, ...) log_message(FATAL, msg, ##__VA_ARGS__)
-#define ERROR(msg, ...) log_message(ERROR, msg, ##__VA_ARGS__)
-#define DEBUG(msg, ...) log_message(DEBUG, msg, ##__VA_ARGS__)
-#define INFO(msg, ...) log_message(INFO, msg, ##__VA_ARGS__)
-#define TRACE(msg, ...) log_message(TRACE, msg, ##__VA_ARGS__)
+#define FATAL(msg, ...) log_message(LOG_LEVEL_FATAL, msg, ##__VA_ARGS__)
+#define ERROR(msg, ...) log_message(LOG_LEVEL_ERROR, msg, ##__VA_ARGS__)
+#define DEBUG(msg, ...) log_message(LOG_LEVEL_DEBUG, msg, ##__VA_ARGS__)
+#define INFO(msg, ...) log_message(LOG_LEVEL_INFO, msg, ##__VA_ARGS__)
+#define TRACE(msg, ...) log_message(LOG_LEVEL_TRACE, msg, ##__VA_ARGS__)
