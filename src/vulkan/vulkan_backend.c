c3c52a825d4869d33b914b0c56b7014d300ef9ab
GalacticPast <192919385+GalacticPast@users.noreply.github.com>
2024-12-31 02:21:59 -0300

    validation layers

--- src/vulkan/vulkan_backend.c
+++ src/vulkan/vulkan_backend.c
@@ -64,7 +64,7 @@ b8 initialize_vulkan(vulkan_context *context, const char *application_name)
     u32 extension_count = 0;
 
     char **required_extensions_name = array_create(const char *);
-    char  *debug_utils = "VK_EXT_debug_utils";
+    char  *debug_utils = VK_EXT_DEBUG_UTILS_EXTENSION_NAME;
 
     required_extensions_name = array_push_value(required_extensions_name, &debug_utils);
     u32 required_extensions_count = (u32)array_get_length(required_extensions_name);
@@ -101,31 +101,30 @@ b8 initialize_vulkan(vulkan_context *context, const char *application_name)
     }
     INFO("Required vulkan layer extensions found.");
 
-    VkInstanceCreateInfo instance_create_info = {};
-    instance_create_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-    instance_create_info.pNext = 0;
-    instance_create_info.flags = 0;
-    instance_create_info.pApplicationInfo = &app_info;
-#ifdef _DEBUG
-    instance_create_info.enabledLayerCount = 1;
-    instance_create_info.ppEnabledLayerNames = (const char *const *)required_layer_names;
-    instance_create_info.enabledExtensionCount = required_extensions_count;
-    instance_create_info.ppEnabledExtensionNames = (const char *const *)required_extensions_name;
-#endif
-
-    VK_CHECK(vkCreateInstance(&instance_create_info, 0, &context->instance));
-    INFO("Vulkan instance created");
     // INFO: create debug messenger
-#ifdef _DEBUG
     VkDebugUtilsMessengerCreateInfoEXT debug_messenger_create_info = {};
     debug_messenger_create_info.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
     debug_messenger_create_info.pNext = 0;
     debug_messenger_create_info.flags = 0;
-    debug_messenger_create_info.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT |
+    debug_messenger_create_info.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT |
                                                   VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
-    debug_messenger_create_info.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT;
+    debug_messenger_create_info.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT |
+                                              VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
     debug_messenger_create_info.pfnUserCallback = debug_messenger_callback;
     debug_messenger_create_info.pUserData = 0;
+
+    VkInstanceCreateInfo instance_create_info = {};
+    instance_create_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    instance_create_info.pNext = (VkDebugUtilsMessengerCreateInfoEXT *)&debug_messenger_create_info;
+    instance_create_info.flags = 0;
+    instance_create_info.pApplicationInfo = &app_info;
+    instance_create_info.enabledLayerCount = (u32)required_layer_count;
+    instance_create_info.ppEnabledLayerNames = (const char *const *)required_layer_names;
+    instance_create_info.enabledExtensionCount = (u32)required_extensions_count;
+    instance_create_info.ppEnabledExtensionNames = (const char *const *)required_extensions_name;
+
+    VK_CHECK(vkCreateInstance(&instance_create_info, 0, &context->instance));
+    INFO("Vulkan instance created");
 
     // INFO: get the fucntion address because its a extension function
     PFN_vkCreateDebugUtilsMessengerEXT create_debug_utils_messenger =
@@ -137,7 +136,7 @@ b8 initialize_vulkan(vulkan_context *context, const char *application_name)
         return false;
     }
     VK_CHECK(create_debug_utils_messenger(context->instance, &debug_messenger_create_info, 0, &context->debug_messenger));
-#endif
+    INFO("Debug messenger created");
 
     return true;
 }
@@ -145,7 +144,29 @@ b8 initialize_vulkan(vulkan_context *context, const char *application_name)
 VkBool32 debug_messenger_callback(VkDebugUtilsMessageSeverityFlagBitsEXT message_severity, VkDebugUtilsMessageTypeFlagsEXT message_types,
                                   const VkDebugUtilsMessengerCallbackDataEXT *callback_data, void *user_data)
 {
-    INFO("message recived");
+    const char *msg = callback_data->pMessage;
+    switch (message_severity)
+    {
+        case VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT: {
+            TRACE("%s", msg);
+        }
+        break;
+        case VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT: {
+            INFO("%s", msg);
+        }
+        break;
+        case VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT: {
+            WARN("%s", msg);
+        }
+        break;
+        case VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT: {
+            ERROR("%s", msg);
+        }
+        break;
+        case VK_DEBUG_UTILS_MESSAGE_SEVERITY_FLAG_BITS_MAX_ENUM_EXT: {
+        }
+        break;
+    }
     return VK_TRUE;
 }
 
@@ -165,9 +186,8 @@ b8 shutdown_vulkan(vulkan_context *context)
 {
 
     INFO("Destroying debug messenger...");
-    // debug_messenger_destroy(context);
+    debug_messenger_destroy(context);
     INFO("Destroying vulkan instance...");
-    // vkDestroyInstance(context->instance, 0);
-    INFO("Destroyed vulkan instance");
+    vkDestroyInstance(context->instance, 0);
     return true;
 }
