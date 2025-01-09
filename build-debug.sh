833976b3f4defe680182db46b6e9b180a8148c88
GalacticPast <192919385+GalacticPast@users.noreply.github.com>
2025-01-09 03:55:16 -0500

    linux fix

--- build-debug.sh
+++ build-debug.sh
@@ -3,20 +3,7 @@
 echo "building project"
 
 
-CC=clang 
-includes="-Isrc/ -lvulkan"
-assembly="learningVulkan"
-compiler_flags=" -Wall -Wextra -g3 -Wconversion -Wdouble-promotion -Wno-unused-parameter -Wno-unused-function -Wno-sign-conversion -fsanitize=undefined -fsanitize-trap"
-defines="-D_DEBUG"
-
 platform=$(echo "$XDG_SESSION_TYPE")
-
-echo "Building for linux-$platform..."
-
-if [[ $platform == "x11" ]]; then
-    includes="$includes -lX11 -lxcb -lX11-xcb -L/usr/X11R6/lib"
-    defines="$defines -DPLATFORM_LINUX_X11"
-fi
 
 if [[ $platform == "wayland" ]]; then
     mkdir -p src/platform/wayland
@@ -37,9 +24,6 @@ if [[ $platform == "wayland" ]]; then
     fi
 
     cd ../../../
-    
-    includes="$includes -lwayland-client -lrt"
-    defines="$defines -DPLATFORM_LINUX_WAYLAND"
 fi
 
 echo "compiling shaders"
@@ -49,25 +33,4 @@ for file in src/shaders/*.vert; do
     glslc "$file" -o bin/$(basename "$file" .vert).spv
 done 
 
-error=$?
-if [ $error -ne 0 ]; then
-    echo "error compiling shaders"
-    exit 1
-fi
-
-for file in src/shaders/*.frag; do
-    echo glslc ${file} -o bin/$(basename "$file" .frag).spv
-    glslc "$file" -o bin/$(basename "$file" .frag).spv
-done 
-
-error=$?
-if [ $error -ne 0 ]; then
-    echo "error compiling shaders"
-    exit 1
-fi
-
-
-c_file_names=$(find . -type f -name '*.c')
-
-echo $CC $c_file_names $defines -o bin/$assembly $includes $compiler_flags
-time $CC $c_file_names $defines -o bin/$assembly $includes $compiler_flags
+make
